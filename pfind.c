6fb59b85e7c44cc66a21e503e2c124d929e2a8c9
giladvz <gilad191413@gmail.com>
2021-12-23 22:22:01 +0200

    Found out assignment is harder. Changed cvs. Starting rewrite thread functions

--- pfind.c
+++ pfind.c
@@ -6,17 +6,8 @@
 #include <limits.h>
 #include <dirent.h>
 #include <sys/stat.h>
-
-pthread_mutex_t dirMutex;
-pthread_mutex_t threadMutex;
-pthread_cond_t isHeadCV;
-pthread_cond_t isNotHeadCV;
-dhead *directoryList;
-thead *threadSleepList;
-int firstRun = 0;
-atomic_int sleeping = 0;
-atomic_int dead = 0;
-int threadNum;
+#include <errno.h>
+#include <unistd.h>
 
 typedef struct threadNode {
     int id;
@@ -42,12 +33,22 @@ typedef struct directoryHead {
     directoryNode * tail;
 }dhead;
 
-
+dhead *directoryList;
+thead *threadSleepList;
+pthread_t * threads;
+pthread_mutex_t dirMutex;
+pthread_mutex_t threadMutex;
+pthread_cond_t * cvArray;
+pthread_cond_t start;
+atomic_int sleeping = 0;
+atomic_int dead = 0;
+int threadNum;
+int firstRun = 0;
 
 thead * create_sleep_list(void) {
     thead * head = (thead*)malloc(sizeof(thead));
     if(head == NULL) {
-        printf("Error with malloc");
+        perror("Error with malloc");
         exit(1);
     }
     head -> head = NULL;
@@ -58,7 +59,7 @@ thead * create_sleep_list(void) {
 dhead * create_directory_list(void) {
     dhead * head = (dhead*)malloc(sizeof(dhead));
     if(head == NULL) {
-        printf("Error with malloc");
+        perror("Error with malloc");
         exit(1);
     }
     head -> head = NULL;
@@ -66,11 +67,11 @@ dhead * create_directory_list(void) {
     return head;
 }
 
-void add_directory_node(dhead * head, char * fullPath) {
+int add_directory_node(dhead * head, char * fullPath) {
     directoryNode * new_node = (directoryNode *)malloc(sizeof(directoryNode));
     if (new_node == NULL) {
-        printf("Error with malloc");
-        return;
+        perror("Error with malloc");
+        return 0;
     }
     directoryNode * tmp = head -> tail;
     if (tmp != NULL) {
@@ -83,6 +84,27 @@ void add_directory_node(dhead * head, char * fullPath) {
     new_node -> next = tmp;
     strcpy(new_node -> path,fullPath);
     new_node -> prev = NULL;
+    return 1;
+}
+
+int add_thread_node(thead * head,int id){
+    threadNode * new_node = (threadNode *)malloc(sizeof(threadNode));
+    if (new_node == NULL) {
+        perror("Error with malloc");
+        return 0;
+    }
+    threadNode * tmp = head -> tail;
+    if (tmp != NULL) {
+        tmp -> prev = new_node;
+    }
+    head -> tail = new_node;
+    if (head -> head == NULL) {
+        head -> head = new_node;
+    }
+    new_node -> next = tmp;
+    new_node -> prev = NULL;
+    new_node -> id = id;
+    return 1;
 }
 
 void remove_directory_node(dhead * head) {
@@ -106,14 +128,28 @@ int isDirQEmpty(dhead * head){
     return 0;
 }
 
-void * startDirCheck(void* num){
-    long my_id = (long)num;
-    char * path[PATH_MAX];
-    pthread_cond_wait(isHeadCV);
-    pthread_mutex_lock(dirMutex);
+void * startDirCheck(void* num) {
+    long my_id = (long) num;
+    char *path[PATH_MAX];
+    sleeping++;
+    pthread_cond_wait(&start,&threadMutex);
+    sleeping--;
+    //pthread_mutex_lock(dirMutex);
+
+}
+    /*
     // Fist run, 1 gets the file, the rest go to sleep
     if (firstRun) {
         // Add to thread sleeping list
+        pthread_mutex_lock(threadMutex);
+        add_thread_node(threadSleepList,my_id);
+        pthread_mutex_unlock(threadMutex);
+        if (threadSleepList -> head -> id == my_id) {
+            pthread_cond_wait(isHeadCV);
+        }
+        else {
+            pthread_cond_wait(isNotHeadCV);
+        }
     }
     else {
         firstRun = 1;
@@ -129,54 +165,60 @@ void * startDirCheck(void* num){
     }
     return 0;
 }
-
+*/
 int main(int argc, char* argv[]) {
     long i;
 
-    int isEveryOneAsleep;
-
     pthread_mutex_init(&dirMutex, NULL);
     pthread_mutex_init(&threadMutex, NULL);
-    pthread_cond_init(&isHeadCV, NULL);
-    pthread_cond_init(&isNotHeadCV, NULL);
+    pthread_cond_init(&start,NULL);
 
     if (argc != 4) {
-        printf("There should be 3 paramaters\n");
+        errno = EINVAL;
+        perror("There should be 3 paramaters");
         exit(1);
     }
     threadNum = atoi(argv[3]);
     DIR * check_dir = opendir(argv[1]);
     if (check_dir == NULL) {
-        printf("Directory %s: Permission denied.\n",argv[1]);
+        perror("Root directory is invalid.");
         exit(1);
     }
     if(closedir(check_dir) <0 ) {
-        printf("Error closing dir");
+        perror("Error closing dir");
         exit(1);
     }
 
     // Creating queues
-    directoryList = create_directory_list();
-    threadSleepList = create_sleep_list();
+    if ((directoryList = create_directory_list()) == NULL) {
+        exit(1);
+    }
+    if ((threadSleepList = create_sleep_list()) == NULL) {
+        exit(1);
+    }
     // Adding root directory to queue
-    add_directory_node(directoryList,argv[1]);
+    if(add_directory_node(directoryList,argv[1]) == 0 ) {
+        exit(1);
+    }
 
     pthread_t * threads = (pthread_t *)calloc(threadNum,sizeof(pthread_t));
     for (i=0; i<threadNum;i++){
+        pthread_cond_init(&cvArray[i],NULL);
         if ((pthread_create(&threads[i], NULL, startDirCheck, (void *)i)) < 0) {
-            printf("Error creating threads");
+            perror("Error creating threads");
             exit(1);
         }
     }
-    sleep 1;
-    pthread_cond_broadcast(isHeadCV);
+    while(sleeping != threadNum) {sleep(1);}
+    pthread_cond_broadcast(&start);
 
     for (int i = 0; i < threadNum; i++) {
         pthread_join(threads[i], NULL);
     }
     // Clean up and exit
-    pthread_mutex_destroy(dirMutex);
-    pthread_mutex_destroy(threadMutex);
-    pthread_cond_destroy(isHeadCV);
-    pthread_cond_destroy(isNotHeadCV);
+    pthread_mutex_destroy(&dirMutex);
+    pthread_mutex_destroy(&threadMutex);
+    for (i =0; i<threadNum; i++) {
+        pthread_cond_destroy(&cvArray[i]);
+    }
 }
