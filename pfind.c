32255c405284ff352e8ebb4a45856a321f3dac0c
giladvz <gilad191413@gmail.com>
2021-12-23 14:56:23 +0200

    Added malloc checks in lists. Added user parameters checks. Started threads.

--- pfind.c
+++ pfind.c
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <limits.h>
+#include <dirent.h>
+#include <sys/stat.h>
 
 typdef struct threadHead {
     threadNode * head;
@@ -30,8 +32,9 @@ typedef struct directoryNode {
 } directoryNode;
 
 thead * create_sleep_list(void) {
-    ThreadNode * head = (ThreadNode*)malloc(sizeof(thead));
+    thead * head = (ThreadNode*)malloc(sizeof(thead));
     if(head == NULL) {
+        printf("Error with malloc");
         exit(1);
     }
     head -> head = NULL;
@@ -40,8 +43,9 @@ thead * create_sleep_list(void) {
 }
 
 hhead * create_directory_list(void) {
-    directoryNode * head = (directoryNode*)malloc(sizeof(dhead));
+    dhead * head = (directoryNode*)malloc(sizeof(dhead));
     if(head == NULL) {
+        printf("Error with malloc");
         exit(1);
     }
     head -> head = NULL;
@@ -52,6 +56,7 @@ hhead * create_directory_list(void) {
 void add_directory_node(dhead * head, char * fullPath) {
     directoryNode * new_node = (directoryNode *)malloc(sizeof(directoryNode));
     if (new_node == NULL) {
+        printf("Error with malloc");
         return NULL;
     }
     directoryNode * tmp = head -> tail;
@@ -79,4 +84,28 @@ void remove_directory_node(dhead * head) {
     head -> head = tmp -> prev;
     head -> head -> next = NULL;
     free(tmp);
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 4) {
+        printf("There should be 3 paramaters");
+    }
+    DIR * check_dir = opendir(argv[1]);
+    if (check_dir == NULL) {
+        printf("Directory %s: Permission denied.\n",argv[1]);
+        exit(1);
+    }
+    if(closedir(check_dir) <0 ) {
+        printf("Error closing dir");
+        exit(1);
+    }
+    pthread_t * threadlist = (pthread_t *)calloc(atoi(argv[3]),sizeof(pthread_t));
+
+
+
+    for (int i = 0; i < NUM_THREADS; i++) {
+        pthread_join(threads[i], NULL);
+    }
+    // Clean up and exit
+
 }
