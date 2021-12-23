6d5a9537be914845b2e8daadeadf4eed78ecd196
giladvz <gilad191413@gmail.com>
2021-12-23 13:57:07 +0200

    Changed to double linked lists. Fixed add and remove to directory list.

--- pfind.c
+++ pfind.c
@@ -18,16 +18,18 @@ typdef struct directoryHead {
 typedef struct threadNode {
     int id;
     threadNode  * next;
+    threadNode  * prev;
 
 }threadNode;
 
 typedef struct directoryNode {
-    char * [PATH_MAX];
-    directory_Node  * next;
+    char * path[PATH_MAX];
+    directory_Node * next;
+    directory_Node * prev;
 
 } directoryNode;
 
-static thead * create_sleep_list(void) {
+thead * create_sleep_list(void) {
     ThreadNode * head = (ThreadNode*)malloc(sizeof(thead));
     if(head == NULL) {
         exit(1);
@@ -37,7 +39,7 @@ static thead * create_sleep_list(void) {
     return head;
 }
 
-static thead * create_directory_list(void) {
+hhead * create_directory_list(void) {
     directoryNode * head = (directoryNode*)malloc(sizeof(dhead));
     if(head == NULL) {
         exit(1);
@@ -47,3 +49,34 @@ static thead * create_directory_list(void) {
     return head;
 }
 
+void add_directory_node(dhead * head, char * fullPath) {
+    directoryNode * new_node = (directoryNode *)malloc(sizeof(directoryNode));
+    if (new_node == NULL) {
+        return NULL;
+    }
+    directoryNode * tmp = head -> tail;
+    if (tmp != NULL) {
+        tmp -> prev = new_node;
+    }
+    head -> tail = new_node;
+    if (head -> head == NULL) {
+        head -> head = new_node;
+    }
+    new_node -> next = tmp;
+    new_node -> path = fullpath;
+    new_node -> prev = NULL;
+}
+
+void remove_directory_node(dhead * head) {
+    directoryNode * tmp = head -> head;
+    if (head -> head == head -> tail) {
+        head -> head = NULL;
+        head -> tail = NULL;
+        free(tmp);
+        return;
+    }
+
+    head -> head = tmp -> prev;
+    head -> head -> next = NULL;
+    free(tmp);
+}
